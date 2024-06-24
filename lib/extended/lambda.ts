b99d81d5a6a875c020a24f64e88e84fd550af4d9
wesleykel <wesley.kelly@hotmail.com>
2024-06-24 14:33:15 +0100

    extended lambda with sqs

--- lib/extended/lambda.ts
+++ lib/extended/lambda.ts
@@ -3,39 +3,49 @@ import {
   NodejsFunction,
   NodejsFunctionProps,
 } from "aws-cdk-lib/aws-lambda-nodejs";
-import { IQueue, Queue } from "aws-cdk-lib/aws-sqs";
+import { IQueue, Queue, DeadLetterQueue } from "aws-cdk-lib/aws-sqs";
 import { Construct } from "constructs";
 import { NODATA } from "dns";
 import { NodeRuntime } from "inspector";
-import { boolean, string } from "zod";
+import { boolean, lazy, string } from "zod";
 import { handler } from "../../src/handler";
 import { RetentionDays } from "aws-cdk-lib/aws-logs";
+import { Lazy } from "aws-cdk-lib";
 export interface ExampleConstructProps extends NodejsFunctionProps {
   isTriggerQue: boolean;
 }
 
 export class ExtendedLambda extends NodejsFunction {
   constructor(scope: Construct, id: string, props: ExampleConstructProps) {
+    const getQue = (id: number): DeadLetterQueue => {
+      if (props.isTriggerQue) {
+        const dlk123 = new Queue(scope, `dlx${id}`);
+        const que: DeadLetterQueue = {
+          queue: dlk123,
+          maxReceiveCount: 1,
+        };
+        return que;
+      } else {
+        const dlk = new Queue(scope, `default`);
+        const defaultQueue: DeadLetterQueue = {
+          queue: dlk,
+          maxReceiveCount: 1,
+        };
+        return defaultQueue;
+      }
+    };
+    const { queue } = getQue(1234);
     const lambaProps: ExampleConstructProps = {
       isTriggerQue: false,
       entry: props.entry,
       handler: props.handler,
       logRetention: RetentionDays.ONE_WEEK,
       runtime: Runtime.NODEJS_20_X,
+
+      deadLetterQueue: queue,
     };
 
     super(scope, id, lambaProps);
-
-    if (props.isTriggerQue) {
-      const dlk = new Queue(this, "dlk");
-      const que = new Queue(this, "que", {
-        deadLetterQueue: {
-          queue: dlk,
-          maxReceiveCount: 1,
-        },
-      });
-
-      que.grantSendMessages(this);
-    }
+    lambaProps.deadLetterQueue?.grantSendMessages(this);
   }
 }
